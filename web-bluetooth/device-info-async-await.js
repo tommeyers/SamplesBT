5bf2274b2a1e5999e67c5fbeeb34da761e470eae
tommeyers <tomm@desktop-concepts.com>
2018-09-21 18:49:41 -0400

    More

--- web-bluetooth/device-info-async-await.js
+++ web-bluetooth/device-info-async-await.js
@@ -11,4 +11,30 @@ async function onButtonClick() {
   } catch(error)  {
     log('Argh! ' + error);
   }
+  
+  
+  //======================================================================
+	var rxbuilder = '';
+	function onBTReceive(info) {
+		console.log('Received ' + info.data.byteLength + ' bytes of data: ' 
+		       + convertArrayBufferToDumpString(info.data));
+		var rxstring = convertArrayBufferToString(info.data);
+		rxbuilder += rxstring;
+		if (rxbuilder.charCodeAt(rxbuilder.length - 1) == 13) {
+			var rxdata = rxbuilder.slice(0, -1);
+			console.log('<< Received message: "' + rxdata + '"');
+			rxbuilder = '';
+		}
+		else {
+			console.log('Message is not terminated. Message so far is: "' + rxbuilder + '"');
+		}
+	}
+    //======================================================================
+	function onBTReceiveError(errorInfo) {
+		console.log(errorInfo.errorMessage);
+	}
+    //======================================================================
+	navigator.bluetoothSocket.onReceive.addListener(onBTReceive);
+	navigator.bluetoothSocket.onReceiveError.addListener(onBTReceiveError);
+
 }
