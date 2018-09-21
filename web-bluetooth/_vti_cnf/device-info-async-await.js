183969d34e1c130861002518c71b9db16c5a5d95
tommeyers <tomm@desktop-concepts.com>
2018-09-21 18:38:23 -0400

    More

--- web-bluetooth/_vti_cnf/device-info-async-await.js
+++ web-bluetooth/_vti_cnf/device-info-async-await.js
@@ -1,9 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|20 Sep 2018 22:44:08 -0000
+vti_timelastmodified:TR|21 Sep 2018 22:37:15 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|DESKTOP-RS1DU4G\\tomm
 vti_modifiedby:SR|DESKTOP-RS1DU4G\\tomm
 vti_timecreated:TR|20 Sep 2018 22:44:08 -0000
-vti_cacheddtm:TX|20 Sep 2018 22:44:08 -0000
-vti_filesize:IR|1080
 vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|20 Sep 2018 22:44:08 -0000
+vti_cacheddtm:TX|21 Sep 2018 22:37:15 -0000
+vti_filesize:IR|470
