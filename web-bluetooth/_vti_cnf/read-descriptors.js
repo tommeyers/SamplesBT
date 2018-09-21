8c7b3e7c5f9865b799d08527d95dcc1fa30e3e74
tommeyers <tomm@desktop-concepts.com>
2018-09-21 11:15:38 -0400

    Begin Serial-Serial

---
+++ web-bluetooth/_vti_cnf/read-descriptors.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|20 Sep 2018 22:44:23 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|DESKTOP-RS1DU4G\\tomm
+vti_modifiedby:SR|DESKTOP-RS1DU4G\\tomm
+vti_timecreated:TR|20 Sep 2018 22:44:23 -0000
+vti_cacheddtm:TX|20 Sep 2018 22:44:23 -0000
+vti_filesize:IR|2750
+vti_backlinkinfo:VX|
